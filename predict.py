32ca439de2c37f319a62305fb0ec8d1413323d53
George Gaarder <ggaarder@foxmail.com>
2018-04-22 15:23:22 +0800

    2018-01-22 4

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 
 def read_csv(csvfilename):
@@ -5,6 +6,29 @@ def read_csv(csvfilename):
         reader = csv.reader(f)
         for row in reader:
             yield row[0], row[1]
+
+def wordlst_gen(docs):
+    words = set()
+
+    for doc in docs:
+        for word in doc.split(' '):
+            words.add(word)
+
+    return words
+
+def doc2vec(doc, wordlst):
+    """todo: bdc"""
+    doc_words = doc.split(' ')
+    return [doc_words.count(i) for i in wordlst]
+
+def predict(docs, traindat):
+    """
+    Note: If predict(doc, traindat), traindat's word list will be generated every call. time-consuming
+
+    Using KNN, with VSM+bdf
+    """
+    words = wordlst_gen(docs + [i[1] for i in traindat])
+    return 'xxx'
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
@@ -15,9 +39,17 @@ if __name__ == '__main__':
     logging.info('Reading training data ...')
     traindat = [[topic, doc] for topic, doc in read_csv(traincsv)]
 
-    logging.info('Examing ...')
-    correct_cnt = 0
-    quiz_sum = 0
+    logging.info('Reading test data ...')
+    questions, correct_answers = [], []
+
     for topic, doc in read_csv(testcsv):
-        quiz_sum += 1
-        if predict(doc, traindat) == topic:
+        questions.append(doc)
+        correct_answers.append(topic)
+
+    logging.info('Examing ...')
+    answers = predict(questions, traindat)
+    correct_cnt = len(
+        [i for i, ans in enumerate(answers) if ans == correct_answers[i]])
+    quiz_sum = len(answers)
+
+    print('ACCURACY: {}'.format(correct_cnt/quiz_sum))
