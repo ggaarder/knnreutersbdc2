1b1ede862c00722735df301c5ec7c551fb9dc306
George Gaarder <ggaarder@foxmail.com>
2018-04-22 15:27:30 +0800

    2018-01-26 5

--- experiment.py
+++ experiment.py
@@ -1,39 +1,46 @@
-import argparse
-import logging
-import sys
-import pandas as pd
-import algo
-import util
 """
 Text Classification
+
 Train/Test: Neuters 21578 LEWISSPLIT
+exam with TRAINCSV and TESTCSV, splited according to the LEWISSPLIT
+attribution in <NEUTERS> of the original .sgm file
+
 Method: KNN, bdc
 todo:
 - the accuracy should at ~ 94%
 - SVM
 """
 
+import argparse
+import logging
+import pandas as pd
+import algo
+import util
+
 def similarity(doc1, doc2, cache, wfunc):
+    """cosine"""
     doc1vec, doc2vec = algo.DocVector(doc1), algo.DocVector(doc2)
 
     terms = set(doc1vec.terms) & set(doc2vec.terms) & set(cache['term'])
     terms = sorted(terms)
 
     if wfunc == 'tfidf':
-        w = [float(cache[cache.term == t]['idf']) for t in terms]
+        weights = [float(cache[cache.term == t]['idf']) for t in terms]
 
-    doc1vec = [doc1vec.get_tf(terms[i])*w[i] for i in range(len(terms))]
-    doc2vec = [doc2vec.get_tf(terms[i])*w[i] for i in range(len(terms))]
+    doc1vec = [doc1vec.get_tf(terms[i])*weights[i] for i in range(len(terms))]
+    doc2vec = [doc2vec.get_tf(terms[i])*weights[i] for i in range(len(terms))]
 
     return algo.Vector(doc1vec).cos(algo.Vector(doc2vec))
 
 def predict(doc, train, cache, wfunc):
-    return algo.knn([
-        [train.loc[i,'label'],
-         similarity(doc, train.loc[i,'doc'], cache, wfunc)]
+    """predict doc's category"""
+    return algo.knn(5, [
+        [train.loc[i, 'label'],
+         similarity(doc, train.loc[i, 'doc'], cache, wfunc)]
         for i in range(len(train))])
 
 def experiment(test, train, wfunc):
+    """test"""
     cache = pd.read_csv(util.CACHE_FILE)
     actual, expect = [], []
 
@@ -44,31 +51,27 @@ def experiment(test, train, wfunc):
         expect.append(test.loc[i, 'label'])
         actual.append(predict(test.loc[i, 'doc'], train, cache, wfunc))
 
-        logging.info('#{}: {}'.format(i, algo.accuracy(actual, expect)))
+        logging.info('#%d: %f', i, algo.accuracy(actual, expect))
 
-        out.write('{},{}\n', actual[-1], expect[-1])
+        out.write('{},{}\n'.format(actual[-1], expect[-1]))
         out.flush()
 
     out.close()
 
     logging.info('Experiment success')
-    logging.info('Weight Function: {}'.format(wfunc))
-    logging.info('Accuracy: {}'.format(algo.accuracy(actual, expect)))
-    logging.info('Micro F1: {}'.format(algo.micro_f1(actual, expect)))
-    logging.info('Macro F1: {}'.format(algo.macro_f1(actual, expect)))
-    logging.info('Result saved to {}'.format(util.EXPERIMENT_FILE))
+    logging.info('Weight Function: %s', wfunc)
+    logging.info('Accuracy: %f', algo.accuracy(actual, expect))
+    logging.info('Micro F1: %f', algo.micro_f1(actual, expect))
+    logging.info('Macro F1: %f', algo.macro_f1(actual, expect))
+    logging.info('Result saved to %s', util.EXPERIMENT_FILE)
 
 if __name__ == '__main__':
-    """
-    exam with TRAINCSV and TESTCSV, splited according to the LEWISSPLIT
-    attribution in <NEUTERS> of the original .sgm file
-    """
     logging.basicConfig(level=logging.INFO)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--weight-func", type=str,
-        default='tfidf',
-        choices=['tfbdc', 'tfidf'])
-    args = parser.parse_args()
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("-w", "--weight-func", type=str,
+                        default='tfidf',
+                        choices=['tfbdc', 'tfidf'])
+    ARGS = PARSER.parse_args()
 
     experiment(pd.read_csv(util.TEST_CSV), pd.read_csv(util.TRAIN_CSV),
-        args.weight_func)
+               ARGS.weight_func)
